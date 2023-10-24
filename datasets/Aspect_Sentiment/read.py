f4574f41cadea30384f763dda648522771fa10ad
Behzad Shomali <behzad.shomali@gmail.com>
2023-10-24 18:59:42 +0200

    Remove unused files given the new structure of the repo

--- datasets/Aspect_Sentiment/read.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df_train = pd.read_json('train.json', orient='index')
-print(len(df_train)) # 2163
-
-df_test = pd.read_json('test.json', orient='index')
-print(len(df_test)) # 638
